4be56d0f2a45e681793cf1591c1e7d42fac7342c
AmeChanRain <downloadbot007@gmail.com>
2023-08-14 01:32:52 +0000

    zijin: Initial blobs list

--- extract-files.sh
+++ extract-files.sh
@@ -5,6 +5,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/etc/camera/zijin_motiontuning.xml)
+            sed -i 's/xml=version="1.0"/xml version="1.0"/g' "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
