76a5a13eed26c89f97f6bea53db66c74eaed61f5
AmeChanRain <downloadbot007@gmail.com>
2023-08-26 17:30:37 +0000

    zijin: Decommonize camera blobs fixup
    * Original commits:
      https://github.com/Diva-Room/Miku_device_xiaomi_mojin/commit/982ab1ad48fb4090475f22d6e54d5f3116b3b49f
      https://github.com/Diva-Room/Miku_device_xiaomi_mojin/commit/28ddc5113136cb79518e18974603aefdba52a395

--- extract-files.sh
+++ extract-files.sh
@@ -14,6 +14,13 @@ function blob_fixup() {
         vendor/etc/camera/zijin_motiontuning.xml)
             sed -i 's/xml=version="1.0"/xml version="1.0"/g' "${2}"
             ;;
+        vendor/lib64/hw/camera.qcom.so)
+            sed -i "s/\x73\x74\x5F\x6C\x69\x63\x65\x6E\x73\x65\x2E\x6C\x69\x63/\x63\x61\x6D\x65\x72\x61\x5F\x63\x6E\x66\x2E\x74\x78\x74/g" "${2}"
+            ;;
+        vendor/lib64/hw/camera.xiaomi.so)
+            hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/2100805229070094/210080521F2003D5/g" | xxd -r -p > "${EXTRACT_TMP_DIR}/${1##*/}"
+            mv "${EXTRACT_TMP_DIR}/${1##*/}" "${2}"
+            ;;
     esac
 }
 
